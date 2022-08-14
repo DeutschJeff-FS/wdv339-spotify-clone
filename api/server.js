ddca1360df16e4e34b7169660472e6dd40df812e
Jeff Deutsch <88303124+DeutschJeff-FS@users.noreply.github.com>
2022-08-14 10:49:43 -0400

    build: add client key and secret

--- api/server.js
+++ api/server.js
@@ -8,6 +8,14 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
+// declare router(s)
+
+// declare database url
+
+// setup mongoose to mongodb connection
+
+// setup route(s) with base url
+
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}.`);
 });
