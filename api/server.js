532667186539b71e894fd53b4dd332d8fe3dd8f1
Jeff Deutsch <88303124+DeutschJeff-FS@users.noreply.github.com>
2022-08-21 18:09:31 -0400

    add pkgs, update server and .env

--- api/server.js
+++ api/server.js
@@ -2,19 +2,24 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
-
-// declare router(s)
-
 // declare database url
+const DATABASE_URL = process.env.DATABASE_URL;
 
-// setup mongoose to mongodb connection
+// Setup mongoose to MongoDB connection
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log(`Database Connection Established.`));
 
-// setup route(s) with base url
+const spotifyRouter = require("./routes/spotifyRoutes");
+app.use("/spotify/v1", spotifyRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}.`);
